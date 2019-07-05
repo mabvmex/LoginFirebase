13534ba7075b90765545caf414831c05c3b59338
mabvmex <mabvmex@gmail.com>
2019-07-05 15:56:35 -0500

    guard cuando estáloggueado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import { LoguedGuard } from './guards/logued.guard';
+
 
 const routes: Routes = [
   { path: 'home'    , component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'registro', component: RegistroComponent},
-  { path: 'login'   , component: LoginComponent},
+  { path: 'registro', component: RegistroComponent, canActivate: [LoguedGuard]},
+  { path: 'login'   , component: LoginComponent, canActivate: [LoguedGuard]},
   { path: '**', redirectTo: 'registro' }
 ];
 
--- src/app/guards/logued.guard.ts
+++ src/app/guards/logued.guard.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+
 import { AuthService } from '../services/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  ruta: string;
+export class LoguedGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate( /* next: ActivatedRouteSnapshot,     state: RouterStateSnapshot*/): boolean {
-    // console.log('El guard entró');
+
+  canActivate(): boolean {
+
     if (this.auth.estaAutenticado()) {
-      return true;
+    this.router.navigateByUrl('home');
     } else {
-      this.router.navigateByUrl('/login');
-      return false;
+      return true;
     }
-    // return this.auth.estaAutenticado();
   }
 }
